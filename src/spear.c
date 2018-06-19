627ced11d47c96a226e88e6710b460223c8317f0
Ivan Radanov Ivanov <ivanradanov@yahoo.co.uk>
2018-06-20 02:16:01 +0900

    Solve spear

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,18 @@ int n;
 int k;
 int A[100000];
 
+int is_ok(int p)
+{
+	if (p == 0)
+		return 1;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += A[i] / p;
+		if (sum >= k)
+			return 1;
+	}
+	return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +24,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+  lb = 0, ub = A[n - 1];
+  while (lb + 1 < ub) {
+	  i = (lb + ub) / 2;
+	  if (is_ok(i))
+		  lb = i;
+	  else
+		  ub = i;
+  }
+  printf("%d\n", lb);
   return 0;
 }
