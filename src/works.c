a28d7198b52af06d50381b3bbc4bdb60469195ec
Ivan Radanov Ivanov <ivanradanov@yahoo.co.uk>
2018-06-22 00:23:11 +0900

    Solve works

--- src/works.c
+++ src/works.c
@@ -4,14 +4,40 @@ int n;
 int k;
 int A[100000];
 
+int is_ok(int p)
+{
+	int sum = 0, num = 1;
+	for (int i = 0; i < n; i++) {
+		sum += A[i];
+		if (sum > p) {
+			if (++num > k)
+				return 0;
+			sum = A[i];
+		}
+	}
+	return 1;
+}
 
 int main(){
   int i, lb, ub;
+  int sum = 0, max = 0;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    sum += A[i];
+    if (max < A[i])
+	    max = A[i];
   }
 
+  lb = max - 1, ub = sum;
+  while (lb + 1 < ub) {
+	  i = (lb + ub) / 2;
+	  if (is_ok(i))
+		  ub = i;
+	  else
+		  lb = i;
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
